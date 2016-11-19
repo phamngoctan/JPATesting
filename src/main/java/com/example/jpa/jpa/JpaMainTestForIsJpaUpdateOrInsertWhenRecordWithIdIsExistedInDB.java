b51626cfe2c88bc0225c10348fca39f2bd362b06
user <user@DESKTOP-MA92V9K>
2016-11-19 16:59:11 +0700

    Add two more test cases for check whether JPA perform insert or update

---
+++ src/main/java/com/example/jpa/jpa/JpaMainTestForIsJpaUpdateOrInsertWhenRecordWithIdIsExistedInDB.java
@@ -0,0 +1,12 @@
+package com.example.jpa.jpa;
+
+public class JpaMainTestForIsJpaUpdateOrInsertWhenRecordWithIdIsExistedInDB {
+
+	// TODO: check if whether JPA perform insert or update a record when that record has
+	// primary key id same as in DB
+	// TODO: check as the same above but the ID is not the primary key
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/main/java/com/example/jpa/jpa/JpaMainTestForMergeAndPersist.java
@@ -0,0 +1,10 @@
+package com.example.jpa.jpa;
+
+public class JpaMainTestForMergeAndPersist {
+	
+	// TODO: test the persist and merge
+	public static void main(String[] args) {
+		
+	}
+	
+}
